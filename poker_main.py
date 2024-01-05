d65de5728e6d6385b86dae53608031ba5c303e3b
LarkMi <1679044480@qq.com>
2024-01-05 10:37:39 +0800

    test

--- poker_main.py
+++ poker_main.py
@@ -13,11 +13,21 @@ def main():
         st.header('德州扑克')
         st.write('User: {}'.format(st.session_state['login']))
     
+    room_list = pickle.load(open('room','rb'))
+    
     room = st.radio(
         "选择游戏房间",
         key="visibility",
-        options=pickle.load(open('room','rb')),
+        options=room_list,
     )
+    
+    new_room = st.text_input(label='创建房间:')
+    if st.button('创建'):
+        if new_room not in room_list:
+            room_list.append(new_room)
+            pickle.dump(room_list,open('room','wb'))
+        else:
+            st.warning('房间已存在')
     
 if __name__ == '__main__':
     if 'room' not in os.listdir():
