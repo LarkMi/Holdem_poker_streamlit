c5003a22169b7174a51f7866f14e06def20d6a86
LarkMi <51182700+LarkMi@users.noreply.github.com>
2024-01-11 11:31:30 +0800

    Update poker_main.py

--- poker_main.py
+++ poker_main.py
@@ -4,9 +4,11 @@ from poker_action import init_server_state
 from streamlit_server_state import server_state, server_state_lock
 from collections import defaultdict
 
-if "rooms" not in server_state:
-    with server_state_lock["rooms"]:
-        server_state.rooms = defaultdict(int)
+
+# Check server health
+if "ROOMS" not in server_state:
+    with server_state_lock["ROOMS"]:
+        server_state.ROOMS = {}
 
 def init():
     if 'state' not in st.session_state:
@@ -16,23 +18,23 @@ def init():
     if 'name' not in st.session_state:
         st.session_state.name = None
 
-if __name__ == '__main__':
-    #st.write(st.session_state)
-    init()
-    with st.sidebar:
-        st.title('德州扑克')
-        if st.session_state.state != 'login':
-            st.header('User: {}'.format(st.session_state.name))
+#if __name__ == '__main__':
+#st.write(st.session_state)
+#init()
+with st.sidebar:
+    st.title('德州扑克')
+    if st.session_state.state != 'login':
+        st.header('User: {}'.format(st.session_state.name))
 
-    if st.button('test'):
-         with server_state_lock["rooms"]:
-            server_state.rooms['1'] += 1
-    
-    '''
-    if st.session_state.state == 'login':
-        login_page()
-    elif st.session_state.state == 'room':
-        room()
+if st.button('test'):
+     with server_state_lock["rooms"]:
+        server_state.rooms['1'] += 1
+
+'''
+if st.session_state.state == 'login':
+    login_page()
+elif st.session_state.state == 'room':
+    room()
     elif st.session_state.state == 'in_room':
         in_room()        
     elif st.session_state.state == 'game':
