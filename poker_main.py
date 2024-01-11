d9d07ca1a1bd6da2749b17b6362d572a0b781f0a
LarkMi <51182700+LarkMi@users.noreply.github.com>
2024-01-11 15:18:30 +0800

    Update poker_main.py for debug

--- poker_main.py
+++ poker_main.py
@@ -3,7 +3,13 @@ from poker_pages import login_page, room, in_room, game
 from poker_action import init_server_state
 from streamlit_server_state import server_state, server_state_lock
 from collections import defaultdict
+from streamlit.runtime.runtime import Runtime
+from streamlit.runtime.scriptrunner import get_script_run_ctx
 
+
+
+session_id = get_script_run_ctx().session_id
+st.write(Runtime.instance()._session_mgr.get_session_info(session_id))  # type: ignore  # noqa: E501
 
 def init():
     if 'state' not in st.session_state:
